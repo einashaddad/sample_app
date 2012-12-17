cc03d487c40eff4a90ef899cd5ef1fba2a7d03a9
Einas Haddad <einas@haddad>
2012-12-16 22:44:54 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Wishlister::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
-
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy',  via: :delete #HTTP Delete (via)
+  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
